5c230de480434d827ba99b91b662b6f02099a196
ljl <lurk007>
2022-01-14 09:25:29 +0800

    更新支持多库同步数据

--- lurk007_pip_whl/db/import_data.py
+++ lurk007_pip_whl/db/import_data.py
@@ -5,15 +5,17 @@
 @time:2021/12/24 2:55 下午
 @description:
 """
-from lurk007_pip_whl.db.mysql_pool import MysqlPool
 from lurk007_pip_whl.time.date import Date
 
-def get_source_count(source_tb_name,source_msp):
+
+def get_source_count(source_tb_name, source_msp):
     msp = source_msp
     sql = F"select count(1) as count from {source_tb_name}"
     source_count = msp.fetchone(sql).get("count")
     return source_count
-def insert_script_log(log_pid,log_script_id,target_tb_name,target_msp):
+
+
+def insert_script_log(log_pid, log_script_id, target_tb_name, target_msp):
     msp = target_msp
     log_start_time = Date.now()
     log_status = 1
@@ -21,11 +23,15 @@ def insert_script_log(log_pid,log_script_id,target_tb_name,target_msp):
     log_table_names = target_tb_name
     sql = "insert into daqian_script_log(`pid`,`start_time`,`script_id`,`status`,`is_runed`,`table_names`) values(%s,%s,%s,%s,%s,%s)"
     msp.execute(sql, (log_pid, log_start_time, log_script_id, log_status, log_is_runed, log_table_names,))
-def update_script_log(log_pid,target_msp):
+
+
+def update_script_log(log_pid, target_msp):
     msp = target_msp
     sql = "update daqian_script_log set end_time = %s,status = 0 where pid = %s"
     msp.execute(sql, (Date.now(), log_pid))
-def get_source_data(source_msp,tb_name, columns, limit=None):
+
+
+def get_source_data(source_msp, tb_name, columns, limit=None):
     msp = source_msp
     if limit is None:
         sql = F"select {columns} from {tb_name}".replace(
@@ -42,7 +48,7 @@ def HexToBytes(string):
     return bytes.fromhex(string)
 
 
-def get_report_detail(tb_code, version_number,target_msp):
+def get_report_detail(tb_code, version_number, target_msp):
     msp = target_msp
     sql = "select a.`code` as report_code,a.uuid as report_uuid,a.id as report_id,a.name as report_name,a.version_number,a.periods_type,a.is_main_report,a.dimension_type,b.is_enclosure,b.is_primary_key,b.is_filename,c.`code` as index_code,c.`name` as index_name from daqian_element_report as a LEFT JOIN daqian_element_report_item as b on a.uuid = b.report_uuid LEFT JOIN daqian_element_index_item as c on b.index_uuid = c.uuid where a.`code`=%s and a.version_number = %s"
     print(sql % (tb_code, version_number,))
@@ -50,7 +56,7 @@ def get_report_detail(tb_code, version_number,target_msp):
     return res
 
 
-def insert_entrance_rule(report_detail,version_number,bgq,target_msp):
+def insert_entrance_rule(report_detail, version_number, bgq, target_msp):
     msp = target_msp
     sql = "insert into daqian_import_data_entrance_rule(`report_id`,`code`,`name`," \
           "`version_number`,`periods_type`,`periods`,`is_main_report`,`data_table_name`," \
@@ -66,11 +72,11 @@ def insert_entrance_rule(report_detail,version_number,bgq,target_msp):
 
     msp.execute(sql, (
         report_id, report_code, report_name, version_number, periods_type, bgq, is_main_report, data_table_name,
-        Date.now(), report_uuid,dimension_type))
+        Date.now(), report_uuid, dimension_type))
 
 
 class SyncSourceData:
-    def __init__(self, table_name, unchanged_columns, columns, compare_columns, data,target_msp):
+    def __init__(self, table_name, unchanged_columns, columns, compare_columns, data, target_msp):
         """
         table_name: 数据写入的表名
         columns: 三方源列名 (排除id, create_time, update_time)
